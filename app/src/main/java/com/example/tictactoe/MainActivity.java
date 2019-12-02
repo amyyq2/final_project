651909bf64f9447b20ed7e5967645d4b8ca78ab2
amyqian <amyyq2@illinois.edu>
2019-12-02 16:27:41 -0600

    Clicker Activity

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -17,8 +17,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private int player2Points;
     private TextView player1;
     private TextView player2;
-    private boolean player1Turn = true;
-    private int turns;
 
 
     @Override
@@ -35,7 +33,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 buttons[i][j].setOnClickListener(this);
             }
         }
-
         Button reset = findViewById(R.id.reset);
         reset.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,70 +46,50 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     @Override
-    public void onClick(View v) {
-        toClicker(v);
-        if (!((Button) v).getText().toString().equals("")) {
-            return;
-        }
-        if (player1Turn) {
-            ((Button) v).setText("X");
-        } else {
-            ((Button) v).setText("O");
-        }
-        turns++;
-        if (checkWin()) {
-            if (player1Turn) {
-                player1Wins();
-            } else {
-                player2Wins();
-            }
-        } else if (turns == 9) {
-            draw();
-        } else {
-            player1Turn = !player1Turn;
-        }
+    public void onClick(View view) {
+        toClicker();
     }
 
-    private boolean checkWin() {
-        String[][] field = new String[3][3];
+    private boolean checkBoardWin() {
+        String[][] board = new String[3][3];
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                field[i][j] = buttons[i][j].getText().toString();
+                board[i][j] = buttons[i][j].getText().toString();
             }
         }
         for (int i = 0; i < 3; i++) {
-            if (field[i][0].equals(field[i][1])
-                && field[i][0].equals(field[i][2])
-                && !field[i][0].equals("")) {
+            if (board[i][0].equals(board[i][1])
+                && board[i][0].equals(board[i][2])
+                && !board[i][0].equals("")) {
                 return true;
             }
         }
         for (int i = 0; i < 3; i++) {
-            if (field[0][i].equals(field[1][i])
-                    && field[0][i].equals(field[2][i])
-                    && !field[0][i].equals("")) {
+            if (board[0][i].equals(board[1][i])
+                    && board[0][i].equals(board[2][i])
+                    && !board[0][i].equals("")) {
                 return true;
             }
         }
-        if (field[0][0].equals(field[1][1])
-                && field[0][0].equals(field[2][2])
-                && !field[0][0].equals("")) {
+        if (board[0][0].equals(board[1][1])
+                && board[0][0].equals(board[2][2])
+                && !board[0][0].equals("")) {
             return true;
         }
-        if (field[0][2].equals(field[1][1])
-                && field[0][2].equals(field[2][0])
-                && !field[0][2].equals("")) {
+        if (board[0][2].equals(board[1][1])
+                && board[0][2].equals(board[2][0])
+                && !board[0][2].equals("")) {
             return true;
         }
         return false;
     }
-    private void player1Wins() {
+    private void player1Win() {
         player1Points++;
         Toast.makeText(this, "Player 1 wins", Toast.LENGTH_SHORT).show();
         updatePoints();
         resetBoard();
     }
-    private void player2Wins() {
+    private void player2Win() {
         player2Points++;
         Toast.makeText(this, "Player 2 wins", Toast.LENGTH_SHORT).show();
         updatePoints();
@@ -132,11 +109,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 buttons[i][j].setText("");
             }
         }
-        turns = 0;
-        player1Turn = true;
     }
-    private void toClicker(View view) {
-        Intent intent = new Intent(this, clicker.class);
+    private void toClicker() {
+        Intent intent = new Intent(this, ClickerActivity.class);
         startActivity(intent);
     }
 }
