b0a73b7bf4ab87c369147a255fa9f46a4d44d9d4
tubaj <jcmoore2@illinois.edu>
2019-12-07 14:39:55 -0600

    Added the Win popup

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.tictactoe;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -124,13 +126,41 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
     private void player1Win() {
         player1Points++;
-        Toast.makeText(this, "Player 1 wins", Toast.LENGTH_SHORT).show();
+        //Toast.makeText(this, "Player 1 wins", Toast.LENGTH_SHORT).show();
+        new AlertDialog.Builder(this)
+                .setTitle("Player 1 wins this game!")
+                // Specifying a listener allows you to take an action before dismissing the dialog.
+                // The dialog is automatically dismissed when a dialog button is clicked.
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Continue with delete operation
+                    }
+                })
+
+                // A null listener allows the button to dismiss the dialog and take no further action.
+                //.setNegativeButton(android.R.string.no, null)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
         updatePoints();
         resetBoard();
     }
     private void player2Win() {
         player2Points++;
-        Toast.makeText(this, "Player 2 wins", Toast.LENGTH_SHORT).show();
+        //Toast.makeText(this, "Player 2 wins", Toast.LENGTH_SHORT).show();
+        new AlertDialog.Builder(this)
+                .setTitle("Player 2 wins this game!")
+                // Specifying a listener allows you to take an action before dismissing the dialog.
+                // The dialog is automatically dismissed when a dialog button is clicked.
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Continue with delete operation
+                    }
+                })
+
+                // A null listener allows the button to dismiss the dialog and take no further action.
+                //.setNegativeButton(android.R.string.no, null)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
         updatePoints();
         resetBoard();
     }
